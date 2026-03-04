d19880a20dde07cc77727a7aff900267566ad027
Sarika <sarikas9266@gmail.com>
2026-03-04 20:30:52 +0530

    day 1 : first python program

--- day1_hello.py
+++ day1_hello.py
@@ -1,6 +1,6 @@
 #python basics 
 print("Hello there, I'm Sarika and heres my data from march")#printing statement 
-#basics 
+#basics- task 1 ----------------------------------------------------------------------
 name = "Sarika"
 age = 21
 branch = "AI and DS"
@@ -9,3 +9,11 @@ print("My name is :",name)
 print("My age is :",age)
 print("My branch is :",branch)
 print("My CGPA is :",cgpa)
+
+#task 2 -------------------------------------------------------------------------------------
+print("\n-- User input section --")
+user_name = input("Enter your name :")
+user_age = input("Enter your age :")
+
+print("Hello",user_name,"!")
+print("now you are",user_age,"years old!")
