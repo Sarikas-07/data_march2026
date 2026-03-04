cf212b4f046e5f9cbe74bac1e18805a7108e78bc
Sarika <sarikas9266@gmail.com>
2026-03-04 20:03:10 +0530

    day 1 : first python program

--- day1_hello.py
+++ day1_hello.py
@@ -1,1 +1,11 @@
-print("Hello there, I'm Sarika and heres my data from march")
+#python basics 
+print("Hello there, I'm Sarika and heres my data from march")#printing statement 
+#basics 
+name = "Sarika"
+age = 21
+branch = "AI and DS"
+cgpa =7.7
+print("My name is :",name)
+print("My age is :",age)
+print("My branch is :",branch)
+print("My CGPA is :",cgpa)
